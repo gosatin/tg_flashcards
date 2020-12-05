73997cf504665ad586c29e54a61c94ca3728fd0f
gosatin <Corova85@gh>
2020-12-05 19:40:56 +0200

    init commit

--- config_env.sh
+++ config_env.sh
@@ -1,2 +1,3 @@
-pip install telebot
-pip install telethon
+#pip install telebot
+#pip install telethon
+pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
--- test2.py
+++ test2.py
@@ -1,9 +1,8 @@
 import requests
+import configparser
 
-def telegram_bot_sendtext(bot_message):
 
-    bot_token = ''
-    bot_chatID = ''
+def telegram_bot_sendtext(bot_message, bot_token, bot_chatID):
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
     response = requests.get(send_text)
@@ -11,5 +10,9 @@ def telegram_bot_sendtext(bot_message):
     return response.json()
 
 
-test = telegram_bot_sendtext("Testing Telegram bot")
+config = configparser.ConfigParser()
+config.read('bot.properties')
+
+test = telegram_bot_sendtext("Testing Telegram bot", config.get("BotSection", "bot_token"),
+                             config.get("BotSection", "bot_chatID"))
 print(test)
